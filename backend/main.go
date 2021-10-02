79dece2b432361ae2b8fdf44946b0f61ee391386
icedvigilante <wm.ryan.mooney@gmail.com>
2021-10-02 12:38:32 -0800

    Defined .env data. Defied gofiber and templates

---
+++ backend/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+	"github.com/simpleittools/repair_depot/backend/config"
+	"log"
+)
+
+func main() {
+	PORT := config.EnvVariables("PORT")
+
+	// Dev Status
+	status := config.EnvVariables("STATUS")
+	switch {
+	case status == "Development":
+		fmt.Println("The current status is Development")
+	case status == "Production":
+		fmt.Println("You are in production")
+	default:
+		log.Fatalf("Status is undefined. Please modify your .env file and set your Status Variable. Please match " +
+			"the variable options as defined in the case statements per the main.go Dev Status section")
+	}
+
+	// GoFiber template Engine
+	engine := html.New("./views", ".gohtml")
+	if status == "Development" {
+		engine.Reload(true)
+		engine.Debug(true)
+	}
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	fmt.Println(fmt.Sprintf("Starting application, available at http://localhost%s", PORT))
+	log.Fatal(app.Listen(PORT))
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/simpleittools/repair_depot
 
 go 1.16
+
+require (
+	github.com/gofiber/fiber/v2 v2.19.0 // indirect
+	github.com/gofiber/template v1.6.17 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
