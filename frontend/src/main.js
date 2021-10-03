34a00b479fcca1dd297c576c21b0a8d12b201186
icedvigilante <wm.ryan.mooney@gmail.com>
2021-10-03 14:18:33 -0800

    modify tailwind.css for @apply types

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import '@/index.css'
+// import '@/index.css'
 import './assets/tailwind.css'
 // import '@/assets/vendor/bulma/css/bulma.min.css'
 
--- frontend/src/views/Clients.vue
+++ frontend/src/views/Clients.vue
@@ -1,36 +1,30 @@
 <template>
   <ul class="flex border-b">
     <li class="mr-1" :class="{'-mb-px' : newClient}">
-      <button class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" :class="{'border-l border-t border-r rounded-t border-gray-400 text-blue-700 font-bold': newClient}" @click="newClientButton">New Client</button>
+      <button class="tab-button" :class="{'tab-selected': newClient}" @click="newClientButton">New Client</button>
     </li>
     <li class="mr-1" :class="{'-mb-px' : searchClient}">
-      <button class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" :class="{'border-l border-t border-r rounded-t border-gray-400 text-blue-700 font-bold': searchClient}" @click="searchClientButton">Search Clients</button>
-    </li>
-    <li class="mr-1">
-      <a class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">Tab</a>
-    </li>
-    <li class="mr-1">
-      <a class="bg-white inline-block py-2 px-4 text-gray-400 font-semibold" href="#">Tab</a>
+      <button class="tab-button" :class="{'tab-selected': searchClient}" @click="searchClientButton">Search Clients</button>
     </li>
   </ul>
   <div v-if="newClient">
 
       <form class="w-full max-w-md" @submit.prevent="handleSave">
         <div class="flex items-center border-teal-500 py-2">
-          <input type="text" class="form-input focus:shadow-outline" v-model="clientDetails.firstName" placeholder="First Name">
+          <input type="text" class="form-input" v-model="clientDetails.firstName" placeholder="First Name">
         </div>
         <div class="flex items-center border-teal-500 py-2">
-          <input type="text" class="form-input focus:shadow-outline" v-model="clientDetails.lastName" placeholder="Last Name">
+          <input type="text" class="form-input" v-model="clientDetails.lastName" placeholder="Last Name">
         </div>
         <div class="flex items-center border-teal-500 py-2">
-          <input type="text" class="form-input focus:shadow-outline" v-model="clientDetails.phone1" placeholder="Primary Phone Number">
+          <input type="text" class="form-input" v-model="clientDetails.phone1" placeholder="Primary Phone Number">
 
         </div>
         <div class="flex items-center border-teal-500 py-2">
-          <input type="text" class="form-input focus:shadow-outline" v-model="clientDetails.phone2" placeholder="Secondary Phone Number">
+          <input type="text" class="form-input" v-model="clientDetails.phone2" placeholder="Secondary Phone Number">
         </div>
         <div class="flex items-center border-teal-500 py-2">
-          <input type="text" class="form-input focus:shadow-outline" v-model="clientDetails.email" placeholder="Email">
+          <input type="text" class="form-input" v-model="clientDetails.email" placeholder="Email">
         </div>
         <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Save</button>
         <button type="reset" @click="clearForm" class="cursor-pointer bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 ml-1 rounded">Clear</button>
